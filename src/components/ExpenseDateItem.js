4171ef8e2f10b540098a9b7ea2f66d6143b73023
James Jasenia <jamesjasenia@Jamess-MBP.modem>
2021-09-11 21:44:44 +1000

    Included css code

--- src/components/ExpenseDateItem.js
+++ src/components/ExpenseDateItem.js
@@ -1,13 +1,15 @@
+import './ExpenseDateItem.css'
+
 const ExpenseDateItem = (props) => {
   const month = props.date.toLocaleString("en-US", { month: "long" });
   const year = props.date.getFullYear();
   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
 
   return (
-    <div>
-      <div>{month}</div>
-      <div>{day}</div>
-      <div>{year}</div>
+    <div className='expense-date'>
+      <div className='expense-date__month'>{month}</div>
+      <div className='expense-date__date'>{day}</div>
+      <div className='expense-date__year'>{year}</div>
     </div>
   );
 };
