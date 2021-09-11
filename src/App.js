60020278f27ecf1a8e793ee32cc4c5facdfdc2b0
James Jasenia <jamesjasenia@Jamess-MBP.modem>
2021-09-11 21:04:23 +1000

    Passing values into props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import ExpenseItem from './components/ExpenseItem'
+import ExpenseItem from "./components/ExpenseItem";
 
 const App = () => {
-  return <ExpenseItem />
-}
+
+  const expenses = [
+    { title: 'Car Insurance', amount: 200.00, date: new Date(2021, 2, 20)},
+    { title: 'Health Insurance', amount: 150.00, date: new Date(2021, 9, 28)},
+    { title: 'House Insurance', amount: 3400.00, date: new Date(2021, 1, 28)},
+  ];
+
+  return (
+    <div>
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
+      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
+      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
+    </div>
+  );
+};
 
 export default App;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,13 @@
 import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
-
-    const expenseDate = new Date(2021, 2, 28);
-    const expenseTitle = 'Car Insurance';
-    const price = '$210.00c'
-
+const ExpenseItem = (props) => {
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{price}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}c</div>
       </div>
     </div>
   );
