b83ebeb9c2a0192e749bf2f9b5f8f7fbf73c4bb5
James Jasenia <jamesjasenia@Jamess-MBP.modem>
2021-09-11 21:55:17 +1000

    Testing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import ExpenseTable from './components/ExpenseTable'
 
 const App = () => {
 
@@ -9,11 +9,7 @@ const App = () => {
   ];
 
   return (
-    <div>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
-    </div>
+    <ExpenseTable expenses={expenses} />
   );
 };
 
