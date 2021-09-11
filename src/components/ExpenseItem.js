cc3fa22e2435c72dcfc8cd9560048bfbc44916f1
James Jasenia <jamesjasenia@Jamess-MBP.modem>
2021-09-11 21:18:29 +1000

    Configured year view

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,8 +2,17 @@ import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
 
+  const month = props.date.toLocaleString('en-US', { month: "long" });
+  const year = props.date.getFullYear();
+  const day = props.date.toLocaleString('en-US', { day: "2-digit" });
+
   return (
     <div className="expense-item">
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
       <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
