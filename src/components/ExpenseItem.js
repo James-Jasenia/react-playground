88acad6ca6d736fb56619a4adfb8430019deee27
James Jasenia <jamesjasenia@Jamess-MBP.modem>
2021-09-11 21:41:42 +1000

    Fought a battle with props, emerged victorious

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,11 @@
 import "./ExpenseItem.css";
+import ExpenseDateItem from './ExpenseDateItem';
 
 const ExpenseItem = (props) => {
-
-  const month = props.date.toLocaleString('en-US', { month: "long" });
-  const year = props.date.getFullYear();
-  const day = props.date.toLocaleString('en-US', { day: "2-digit" });
-
+  
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
-      </div>
-      <div>{props.date.toISOString()}</div>
+      <ExpenseDateItem date = {props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}c</div>
