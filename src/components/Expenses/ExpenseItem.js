d5ae1a1f6aa727ce35ed23fbac73e08bec911583
James Jasenia <jamesjasenia@Jamess-MBP.modem>
2021-10-11 16:21:13 +1100

    Added useState
    'useState' is Reacts way of managing changes in state. The convention for destructing the retunr value of a useState functions is as follows:
    
    const [title, setTitle] = useState(INTIAL_VALUE);

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,27 @@
+import React, { useState } from 'react';
 import "./ExpenseItem.css";
 import Card from '../UI/Card'
 import ExpenseDateItem from './ExpenseDateItem';
 
 const ExpenseItem = (props) => {
-  
+  const [title, setTitle] = useState(props.title);
+
+  const buttonClicked = () => {
+    setTitle("James");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDateItem date = {props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}c</div>
       </div>
+      <button onClick={buttonClicked}>Change title</button>
     </Card>
   );
 };
+
+
 
 export default ExpenseItem;
