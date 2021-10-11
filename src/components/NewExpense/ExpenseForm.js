81d2b475aa4d31dd94e82cac0cc1004ff061c53d
James Jasenia <jamesjasenia@Jamess-MBP-2.modem>
2021-10-11 20:53:48 +1100

    Added alternative to having multiple useState objects... don't like it.

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,20 +1,52 @@
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+
+  const [title, setTitle] = useState('');
+  const [amount, setAmount] = useState('');
+  const [date, setDate] = useState('');
+
+// You can use an object in useSate, however, you will need to update every property in the object every single time regardless if the invidual propety changes or not.
+//   const [userInput, setUserInput] = useState({
+//       title: '',
+//       amount: '',
+//       date: ''
+//   })
+
+// const titleChangeHandler = (event) => {
+//     setTitle({
+//         ...userInput,
+//         title: event.target.value
+//     });
+//   };
+
+  const titleChangeHandler = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const amountChangedHandler = (event) => {
+    setAmount(event.target.value);
+  };
+
+  const dateChangedHandler = (event) => {
+    setDate(event.target.value);
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input type="number" min="0.01" step="0.01" onChange={amountChangedHandler}/>
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" />
+          <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangedHandler}/>
         </div>
       </div>
       <div className="new-expense__actions">
