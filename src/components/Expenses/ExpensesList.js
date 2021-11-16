91e147a7a5a392235d0d2dd66f04c0374b9e85e1
James <james.jasenia@gmail.com>
2021-11-17 08:27:16 +1100

    Added conditional statement that returns JSX cleaner.

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -1,21 +1,25 @@
 import React from "react";
 import ExpenseItem from "./ExpenseItem";
+import "./ExpensesList.css";
 
 const ExpensesList = (props) => {
-  let expenseContent = <p>No expense found.</p>;
-
-  if (props.items.length > 0) {
-    expenseContent = props.items.map((filteredExpense) => (
-      <ExpenseItem
-        key={filteredExpense.id}
-        title={filteredExpense.title}
-        amount={filteredExpense.amount}
-        date={filteredExpense.date}
-      />
-    ));
+  if (props.items.length === 0) {
+    return <h2 className="expenses-list__fallback">No expense found.</h2>;
+  } else {
+    return (
+      <ul className="expenses-list">
+        {props.items.map((filteredExpense) => (
+          <ExpenseItem
+            key={filteredExpense.id}
+            title={filteredExpense.title}
+            amount={filteredExpense.amount}
+            date={filteredExpense.date}
+          />
+        ))}
+        ;
+      </ul>
+    );
   }
-
-  return expenseContent;
 };
 
 export default ExpensesList;
